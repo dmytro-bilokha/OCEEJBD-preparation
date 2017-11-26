512aac9cca7bf72a932407603de1ae38113adcbd
Dmytro Bilokha <dmytro@posteo.net>
2017-11-26 08:51:41 +0200

    Removed @Dependent workaround, CDI injection works without it

--- jmsbytimer/jmsbytimer-web/src/main/java/com/dmytrobilokha/jmsbytimer/web/InfoServlet.java
+++ jmsbytimer/jmsbytimer-web/src/main/java/com/dmytrobilokha/jmsbytimer/web/InfoServlet.java
@@ -4,7 +4,6 @@ import com.dmytrobilokha.jmsbytimer.ejb.MessageStorage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
-@Dependent
 @WebServlet(urlPatterns = "/")
 public class InfoServlet extends HttpServlet {
 
